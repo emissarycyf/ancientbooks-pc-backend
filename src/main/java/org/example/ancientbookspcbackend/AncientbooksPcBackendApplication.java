240bbc731eefb9ce28eef0f2235067204c5804a8
崔雨帆 <emissary_cyf@outlook.com>
2026-09-04 21:59:36 +0800

    init: 项目初始化，基础结构搭建

---
+++ src/main/java/org/example/ancientbookspcbackend/AncientbooksPcBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.ancientbookspcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientbooksPcBackendApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(AncientbooksPcBackendApplication.class, args);
+  }
+  
+}
